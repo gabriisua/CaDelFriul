c653cdaa67b57b5fc9b98fa637e0216d51500723
gabrielesuardi <suardigabriele96@gmail.com>
2026-07-03 11:36:26 +0200

    Initial commit: basic structure for the new project

---
+++ be-cadelfriul/src/main/java/com/cadelfriul/backend/BeCadelfriulApplication.java
@@ -0,0 +1,13 @@
+package com.cadelfriul.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeCadelfriulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeCadelfriulApplication.class, args);
+	}
+
+}
